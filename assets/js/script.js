e8fa02e0c117f46fe150d6f10eb795d357f10810
Zain <zainharoon890@gmail.com>
2021-03-22 17:51:42 +0500

    Implementation of AJAX
    Implemented $.ajax to get recipies from "https://usman-recipes.herokuapp.com/api/recipes" and converted into list items

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+$(function(){
+    $("#load").click(function(){
+        console.log("BTN CLICKED")
+        loadRecipies();
+    });
+    
+});
+function loadRecipies(){
+    $("#load").attr('disabled','true')
+    $.ajax({
+        url:"https://usman-recipes.herokuapp.com/api/recipes",
+        method:"GET",
+        success:function(response){
+            console.log(response)
+            var tempul=document.createElement("ul");
+            // tempul.innerHTML='Recipies:'
+            tempul.style.fontSize='20px'
+            for (let index = 0; index < response.length; index++) {
+                var templi = document.createElement("li");
+                templi.style.fontSize='20px'
+                // templi.append(response[index].title);
+                templi.innerHTML=`<h4>${response[index].title}</h4><p>${response[index].body}</p>`;
+                tempul.append(templi)
+            }
+            $('#recipies').append(tempul);
+        }
+    })
+}
