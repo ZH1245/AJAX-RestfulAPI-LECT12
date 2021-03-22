8f784eea18b8cb14ab106c9bfb8ba971bb21d47c
Zain <zainharoon890@gmail.com>
2021-03-22 18:08:31 +0500

    button disabled added to avoid multiple ul

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,12 +11,14 @@ function loadRecipies(){
         url:"https://usman-recipes.herokuapp.com/api/recipes",
         method:"GET",
         success:function(response){
-            console.log(response)
+            // $("ul").empty()
             var tempul=document.createElement("ul");
+            tempul.id='btn_disabled'
             // tempul.innerHTML='Recipies:'
             tempul.style.fontSize='20px'
             for (let index = 0; index < response.length; index++) {
                 var templi = document.createElement("li");
+                templi.id='recipies-li'
                 templi.style.fontSize='20px'
                 // templi.append(response[index].title);
                 templi.innerHTML=`<h4>${response[index].title}</h4><p>${response[index].body}</p>`;
